75ceb8480b8bdbd22ad57b6886c574b6b6dbb368
EdwinIvanS <sedwin118@hotmail.com>
2023-03-12 19:10:28 -0500

    Implementacion IAccesoDatos

--- src/mx/com/gm/peliculas/datos/IAccesoDatos.java
+++ src/mx/com/gm/peliculas/datos/IAccesoDatos.java
@@ -1,6 +1,14 @@
 
 package mx.com.gm.peliculas.datos;
 
+import java.util.List;
+import mx.com.gm.peliculas.domain.Pelicula;
+
 public interface IAccesoDatos {
-    
+    boolean exite(String nombreArchivo);
+    List<Pelicula> listar(String nombrePelicula);
+    void escribir(Pelicula pelicula, String nombreArchivo, boolean anexar);
+    String buscar(String nombreArchivo, String buscar);
+    void crear(String nombreArchivo);
+    void borrar(String nombreArchivo);    
 }
